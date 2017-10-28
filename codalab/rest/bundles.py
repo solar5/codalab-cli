40284ff19a7d3b3de98660e03c3f53aec196a026
Fabian Chan <fabianc@stanford.edu>
2017-10-28 01:10:03 -0700

    Oops

--- codalab/rest/bundles.py
+++ codalab/rest/bundles.py
@@ -387,7 +387,7 @@ def _fetch_bundle_contents_info(uuid, path=''):
     }
 
 
-@put('/bundles/<uuid:re:%s>/netcat/<port:port>/' % spec_util.UUID_STR, name='netcat_bundle')
+@put('/bundles/<uuid:re:%s>/netcat/<port:int>/' % spec_util.UUID_STR, name='netcat_bundle')
 def _netcat_bundle(uuid, port):
     check_bundles_have_all_permission(local.model, request.user, [uuid])
     bundle = local.model.get_bundle(uuid)
