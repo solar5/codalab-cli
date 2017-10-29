2cb5fd8ab2bedc59b2e634347bd9558b91ba4cbf
Fabian Chan <fabianc@stanford.edu>
2017-10-28 23:27:25 -0700

    asdf

--- codalab/rest/bundles.py
+++ codalab/rest/bundles.py
@@ -390,13 +390,14 @@ def _fetch_bundle_contents_info(uuid, path=''):
 @put('/bundles/<uuid:re:%s>/netcat/<port:int>/' % spec_util.UUID_STR, name='netcat_bundle')
 @get('/bundles/<uuid:re:%s>/netcat/<port:int>/' % spec_util.UUID_STR, name='netcat_bundle')
 @post('/bundles/<uuid:re:%s>/netcat/<port:int>/' % spec_util.UUID_STR, name='netcat_bundle')
+@delete('/bundles/<uuid:re:%s>/netcat/<port:int>/' % spec_util.UUID_STR, name='netcat_bundle')
 def _netcat_bundle(uuid, port):
     check_bundles_have_all_permission(local.model, request.user, [uuid])
-    abort(httplib.NOT_FOUND, repr(request))
+    abort(httplib.NOT_FOUND, repr(request.environ))
     bundle = local.model.get_bundle(uuid)
     if bundle.state in State.FINAL_STATES:
         abort(httplib.FORBIDDEN, 'Cannot netcat bundle, bundle already finalized.')
-    info = local.download_manager.netcat(uuid, port, request.json['message'] if request.json else "")
+    info = local.download_manager.netcat(uuid, port, json.dumps(request.environ))
     return {
         'data': info
     }
