c1862227da473eca27e0300a14bcaff9b69e5a98
Fabian Chan <fabianc@stanford.edu>
2017-10-30 01:52:47 -0700

    adsf

--- codalab/rest/bundles.py
+++ codalab/rest/bundles.py
@@ -388,16 +388,16 @@ def _fetch_bundle_contents_info(uuid, path=''):
     }
 
 
-@put('/bundles/<uuid:re:%s>/netcat/<port:int>/' % spec_util.UUID_STR, name='netcat_bundle')
-@get('/bundles/<uuid:re:%s>/netcat/<port:int>/' % spec_util.UUID_STR, name='netcat_bundle')
-@post('/bundles/<uuid:re:%s>/netcat/<port:int>/' % spec_util.UUID_STR, name='netcat_bundle')
-@delete('/bundles/<uuid:re:%s>/netcat/<port:int>/' % spec_util.UUID_STR, name='netcat_bundle')
-def _netcat_bundle(uuid, port):
+@put('/bundles/<uuid:re:%s>/netcat/<port:int>/<path:path>' % spec_util.UUID_STR, name='netcat_bundle')
+@get('/bundles/<uuid:re:%s>/netcat/<port:int>/<path:path>' % spec_util.UUID_STR, name='netcat_bundle')
+@post('/bundles/<uuid:re:%s>/netcat/<port:int>/<path:path>' % spec_util.UUID_STR, name='netcat_bundle')
+@delete('/bundles/<uuid:re:%s>/netcat/<port:int>/<path:path>' % spec_util.UUID_STR, name='netcat_bundle')
+def _netcat_bundle(uuid, port, path=''):
     check_bundles_have_all_permission(local.model, request.user, [uuid])
     bundle = local.model.get_bundle(uuid)
     if bundle.state in State.FINAL_STATES:
         abort(httplib.FORBIDDEN, 'Cannot netcat bundle, bundle already finalized.')
-    info = local.download_manager.netcat(uuid, port, json.dumps(request.environ))
+    info = local.download_manager.netcat(uuid, port, json.dumps(request.environ, skip_keys=True))
     return {
         'data': info
     }
