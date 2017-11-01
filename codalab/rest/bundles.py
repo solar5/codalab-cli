a3073809169064520edea25e84dd5a045ab06b56
Fabian Chan <fabianc@stanford.edu>
2017-10-31 17:15:45 -0700

    adsf

--- codalab/rest/bundles.py
+++ codalab/rest/bundles.py
@@ -407,6 +407,7 @@ def _netcat_bundle(uuid, port, path=''):
     # Save cookie in client
     cookie = NetCatCookie('/bundles/{}/{}/'.format(uuid, port), max_age=30 * 24 * 60 * 60)
     cookie.save()
+    response.set_cookie('codalab_netcat', '/bundles/{}/{}/'.format(uuid, port))
 
     try:
         request.path_shift(4)
@@ -418,6 +419,7 @@ def _netcat_bundle(uuid, port, path=''):
         rs._status_line = info['status_line']
         rs._headers = info['headers']
         rs.body = info['body']
+        rs.set_cookie('codalab_netcat', '/bundles/{}/{}/'.format(uuid, port))
     finally:
         request.path_shift(-4)
 
